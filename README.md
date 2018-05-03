90a0dbfec5c2952151331cbafd11c3a7788e8a4e
Shi-li-yu <37106577+Shi-li-yu@users.noreply.github.com>
2018-05-03 12:51:15 +0800

    1.完成画圈圈程序 2.十五字游戏

--- README.md
+++
@@ -1,2 +0,0 @@
-# Learning-Process
-Programming-Learning
---
+++ 画图/画图/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 画图
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Random random = new Random();
+        Color getRandomColor()
+        {
+            return Color.FromArgb(
+                random.Next(200),
+                random.Next(200),
+                random.Next(200));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            int x0 = this.Width / 2;
+            int y0 = this.Height / 2;
+            for(int r=0;r<this.Height/2;r+=2)
+            {
+                g.DrawEllipse(
+                    new Pen(getRandomColor(), 1),
+                    x0 - r, y0 - r, r * 2, r * 2);
+            }
+            g.Dispose();
+        }
+    }
+}
